8fc7fe2b23e4fd46c4b2726d4b8456bdc485dc30
Rolando.Cintron <rolando.cintron@gmail.com>
2020-10-11 21:50:31 -0400

    chore: cleaned retro

--- client/src/pages/Retro/retro.js
+++ client/src/pages/Retro/retro.js
@@ -1,9 +1,5 @@
 import React, { useState, useEffect } from "react";
 import API from "../../utils/API";
-import Card from 'react-bootstrap/Card'
-import ButtonGroup from 'react-bootstrap/ButtonGroup'
-import Accordion from 'react-bootstrap/Accordion'
-import CardDeck from 'react-bootstrap/CardDeck'
 import InputGroup from 'react-bootstrap/InputGroup'
 import FormControl from 'react-bootstrap/FormControl'
 import Button from 'react-bootstrap/Button'
@@ -11,104 +7,9 @@ import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
 import Container from 'react-bootstrap/Container'
 import Form from 'react-bootstrap/Form'
-import Table from 'react-bootstrap/Table'
 import io from 'socket.io-client'
-import Badge from 'react-bootstrap/Badge'
 import RetroBody from "../../components/RetroBody"
 
-// const socket = io()
-// socket.on('event', (data) => {
-//   console.log("Event", data);
-// });
-
-// const retroCard = (props)=>{
-//   const [txtBoxValue, setTxtBoxValue] = useState("")
-//   const add_button=(e)=>{
-    
-//     props.set_text([...props.texts, {user:props.session.name, text:txtBoxValue}])
-//     setTxtBoxValue("")
-
-//   }
-//   const rem_button=(e, id)=>{
-//     e.preventDefault();
-//     const retVal = props.texts.filter((x,i)=>i!==id)
-//     props.set_text(retVal)
-//   }
-
-//   return(
-//     <Card sm={12}>
-//     <Card.Body>
-//       <Card.Header>{props.name}</Card.Header>
-      
-//         <Form.Control as="textarea" onChange={e=>setTxtBoxValue(e.target.value)} value={txtBoxValue} rows="3" />
-//         <Button variant="primary" onClick={add_button}>Add</Button>
-//         <Table striped bordered hover>
-//           <tbody>
-//           {props.texts.map((text,id)=>
-//             <tr key={id}>
-//               <td><Badge variant="info">{text.user}</Badge><br/>{text.text}<Button variant="link" onClick={(e)=>{rem_button(e, id)}}>x</Button></td>
-//             </tr>)}
-//           </tbody>
-//         </Table>
-      
-      
-//     </Card.Body>
-//   </Card>
-//   )
-// }
-// const RetroVerticalButtonGroup = (props)=>{
-//   console.log(props)
-//   return(
-//     <ButtonGroup  vertical>
-//       <Accordion defaultActiveKey="0">
-//         <Card>
-//           <Card.Header>
-//             <Accordion.Toggle as={Button} variant="link" eventKey="0">Session Info</Accordion.Toggle>
-//           </Card.Header>
-//           <Accordion.Collapse eventKey="0">
-//             <Card.Body>
-//               <b>Name:</b>{props.session.name}<br/>
-//               <b>Session ID:</b> {props.session.session}
-//             </Card.Body>
-//           </Accordion.Collapse>
-//         </Card>
-//         <Card>
-//           <Card.Header>
-//             <Accordion.Toggle as={Button} variant="link" eventKey="1">
-//               Click me!
-//               </Accordion.Toggle>
-//           </Card.Header>
-//           <Accordion.Collapse eventKey="1">
-//             <Card.Body>Hello! I'm another body</Card.Body>
-//           </Accordion.Collapse>
-//         </Card>
-//       </Accordion>
-//     </ButtonGroup>
-//   )
-// }
-
-// const retroCardRead = (props)=>{
-  
-//   return(
-//     <Card sm={12}>
-//     { /* <Card.Img variant="top" src="holder.js/100px180" /> */ }
-//     <Card.Body>
-//       <Card.Title>{props.name}</Card.Title>
-//         <Table striped bordered hover>
-//           <tbody>
-//           {props.texts.map((text,id)=>
-//             <tr key={id}>
-//               <td>{text}</td>
-              
-//             </tr>)}
-//           </tbody>
-//         </Table>
-      
-      
-//     </Card.Body>
-//   </Card>
-//   )
-// }
 
 const RetroStartForm = (props) =>{
   const [session, setSession] = useState("")
